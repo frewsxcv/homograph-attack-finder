6250621e9a54e0eb5d3d88935b84822854c4aa06
Corey Farwell <coreyf@rwell.org>
2018-01-25 10:23:09 -0500

    print out domain in cyrillic, don’t print out numeric domains

--- run.rb
+++ run.rb
@@ -1,27 +1,30 @@
 require 'set'
 
-cyrillic_lookalikes = Set[
-    'a', # cyrillic: а
-    'c', # cyrillic: с
-    # 'd', # cyrillic: ԁ
-    'e', # cyrillic: е
-    'h', # cyrillic: һ
-    'i', # cyrillic: і
-    'j', # cyrillic: ј
-    # 'k', # cyrillic: ҟ
-    'l', # cyrillic: ӏ
-    # 'm', # cyrillic: м
-    'n', # cyrillic: п
-    'o', # cyrillic: о
-    'p', # cyrillic: р
-    'q', # cryillic: ԛ
-    'r', # cyrillic: г
-    's', # cyrillic: ѕ
-    # 'u', # cyrillic: џ
-    'w', # cyrillic: ԝ
-    'x', # cyrillic: х
-    'y', # cyrillic: у
-].freeze
+# latin → cyrillic
+cyrillic_lookalikes = {
+    'a' => 'а',
+    'c' => 'с',
+    # 'd' => 'ԁ',
+    'e' => 'е',
+    'h' => 'һ',
+    'i' => 'і',
+    'j' => 'ј',
+    # 'k' => 'ҟ',
+    'l' => 'ӏ',
+    # 'm' => 'м',
+    'n' => 'п',
+    'o' => 'о',
+    'p' => 'р',
+    'q' => 'ԛ',
+    'r' => 'г',
+    's' => 'ѕ',
+    # 'u' => 'џ',
+    'w' => 'ԝ',
+    'x' => 'х',
+    'y' => 'у',
+}.freeze
+
+cyrillic_lookalike_latin = Set[*cyrillic_lookalikes.keys].freeze
 
 numbers = Set['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].freeze
 
@@ -31,8 +34,13 @@ file.close
 
 contents.each_line do |line|
     _, line = line.split(',')
-    domain, _ = line.split('.', 2)
-    if Set[*domain.chars].subset?(cyrillic_lookalikes + numbers)
-        puts domain
+    domain, tld = line.split('.', 2)
+    if Set[*domain.chars].subset?(cyrillic_lookalike_latin + numbers)
+        cyrillic_domain = domain
+            .chars
+            .map{|char| cyrillic_lookalikes.fetch(char, char) }
+            .join
+        next if cyrillic_domain == domain # can happen if domain is all numbers
+        puts "http://#{cyrillic_domain}.#{tld}"
     end
 end
